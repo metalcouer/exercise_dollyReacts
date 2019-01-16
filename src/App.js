8fcf9080cac3767d82d708e78cbe996445cde0e3
Aimee Zawacki <aimee@tna.digital>
2019-01-15 18:18:12 -0700

    quote generator layout and basic functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      wisdom: ''
+    }
+  }
+
+  generateQuote = () => {
+
+  }
+
   render() {
+    console.log("all the data", data)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid">
+        <div className="row navbar justify-content-end pb-5">
+          <ul className="nav">
+            <li className="nav-item">
+              <a className="nav-link active" href="http://localhost:3001" target="blank">Hit the Route</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="mailto:aimee.zawacki@galvanize.com">Say Halo!</a>
+            </li>
+          </ul>
+        </div>
+        <div className="row justify-content-center py-5">
+          <div className="col-8 text-center">
+            <h1 className="pb-2">Hello Dolly!</h1>
+            <p className="pb-2">An app for randomly generating bits of Dolly Parton's sea of wisdom.</p>
+            <button className="btn btn-danger btn-lg" onClick={this.generateQuote}>Press Me</button>
+          </div>
+        </div> 
+        <div className="row">
+        {this.state.wisdom ? <h1>{this.state.wisdom}</h1> : ''}
+        </div>
       </div>
     );
   }
